4af75761848d3fa806246a749b4be88d81a7d4f6
NisargMane <76396281+NisargMane@users.noreply.github.com>
2021-02-01 10:31:25 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class add1
+{
+	public static void main(String args[])
+	{
+		int num1,num2,sum;
+		System.out.println("");
+		Scanner scn=new Scanner(System.in);
+		num1=scn.nextInt();
+		num2=scn.nextInt();
+		sum=num1+num2;
+		System.out.print(sum+"");
+	}
+}
